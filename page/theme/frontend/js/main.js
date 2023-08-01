f26dfcf92bb902b22819a371d411cc575f2ac547
dung0811 <103187128+dung2511@users.noreply.github.com>
2023-08-01 23:27:13 +0700

    update

--- page/theme/frontend/js/main.js
+++ page/theme/frontend/js/main.js
@@ -18,18 +18,35 @@ var PROGRAM = (function () {
     };
   };
   var showAns = function () {
-    const questions = document.querySelectorAll(".question");
-    const answer = document.querySelectorAll(".answer");
-    const angleQuestion = document.querySelectorAll(".question i")
-    questions.forEach((question, index) => {
-      question.onclick = function () {
-        answer[index].classList.toggle("active");
-        angleQuestion[index].classList.toggle("active")
-      };
+    var itemQuestions = document.querySelectorAll(".item-question");
+    itemQuestions.forEach(function (item) {
+      var titleQuestion = item.querySelector(".question");
+      var contentQuestion = item.querySelector(".answer");
+      var iconAngleQ = item.querySelector(".question i")
+      titleQuestion.addEventListener("click", function () {
+        if (window.getComputedStyle(contentQuestion).display === "none") {
+          contentQuestion.classList.add("active");
+          var height = contentQuestion.scrollHeight;
+          contentQuestion.style.height = height + "px";
+          iconAngleQ.classList.add("active")
+        } else {
+          contentQuestion.style.height = "0";
+          iconAngleQ.classList.remove("active")
+          contentQuestion.addEventListener(
+            "transitionend",
+            function () {
+              contentQuestion.classList.remove("active");
+            },
+            { once: true }
+          );
+        }
+      });
     });
   };
   var slideAchivements = function () {
-    const sl_achievements_mains = document.querySelector(".sl-achievements_mains");
+    const sl_achievements_mains = document.querySelector(
+      ".sl-achievements_mains"
+    );
     if (sl_achievements_mains.length === 0) return;
     var swiperAchivements = new Swiper(sl_achievements_mains, {
       slidesPerView: 2,
