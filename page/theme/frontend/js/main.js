2999d25bd688b1bff7281637d3bd4d3bdfd3dca0
dung0811 <103187128+dung2511@users.noreply.github.com>
2023-08-01 18:03:18 +0700

    update

--- page/theme/frontend/js/main.js
+++ page/theme/frontend/js/main.js
@@ -1,48 +1,54 @@
 var PROGRAM = (function () {
-  var win = $(window);
-  var html = $("html,body");
   var menuMobile = function () {
-    $("#hamburger").on("click", function () {
-      $("#language-mobile").addClass("active");
-      $("#close-language").toggleClass("hidden");
-    });
-    $("#close-language").on("click", function () {
-      $("#language-mobile").removeClass("active");
-      $("#close-language").toggleClass("hidden");
-    });
-    $(".close-language").on("click", function () {
-      $("#language-mobile").removeClass("active");
-      $("#close-language").toggleClass("hidden");
-    });
+    const hamburger = document.getElementById("hamburger");
+    const languageMoblie = document.getElementById("language-mobile");
+    const closeLanguage = document.getElementById("close-language");
+    const closelanguage = document.querySelector(".close-language");
+    hamburger.onclick = function () {
+      languageMoblie.classList.add("active");
+      closeLanguage.classList.toggle("hidden");
+    };
+    closeLanguage.onclick = function () {
+      languageMoblie.classList.remove("active");
+      closeLanguage.classList.toggle("hidden");
+    };
+    closelanguage.onclick = function () {
+      languageMoblie.classList.remove("active");
+      closeLanguage.classList.toggle("hidden");
+    };
   };
   var showAns = function () {
-    $('.question').on("click" ,function () {
-      $(this).next(".answer").slideToggle();
-    })
-  }
+    const questions = document.querySelectorAll(".question");
+    const answer = document.querySelectorAll(".answer");
+    const angleQuestion = document.querySelectorAll(".question i")
+    questions.forEach((question, index) => {
+      question.onclick = function () {
+        answer[index].classList.toggle("active");
+        angleQuestion[index].classList.toggle("active")
+      };
+    });
+  };
   var slideAchivements = function () {
-    if($('.sl-achievements_mains').length === 0) return;
-    var swiperAchivements = new Swiper(".sl-achievements_mains", {
-      slidesPerView: 1,
-      pagination : {
-        el:".pagination-achievements"
+    const sl_achievements_mains = document.querySelector(".sl-achievements_mains");
+    if (sl_achievements_mains.length === 0) return;
+    var swiperAchivements = new Swiper(sl_achievements_mains, {
+      slidesPerView: 2,
+      pagination: {
+        el: ".pagination-achievements",
       },
-      loop:true,
+      loop: true,
       autoplay: {
         delay: 2000,
         disableOnInteraction: false,
-      }
-    })
-  }
+      },
+    });
+  };
   return {
     _: function () {
-      // gọi lại các function ở đây
       menuMobile();
       showAns();
       slideAchivements();
     },
   };
 })();
-$(document).ready(function () {
-  PROGRAM._();
-});
+PROGRAM._();
